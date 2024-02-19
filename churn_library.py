32f9b8bc28acc93fe4ce032bd57db18b5d714118
Sara <105993032+SaraMAbanmi@users.noreply.github.com>
2024-02-19 10:17:59 +0300

    Add files via upload

---
+++ churn_library.py
@@ -0,0 +1,363 @@
+'''
+
+File Name: churn_library.py
+
+Author(s): Sara Abanmi
+
+Brief Description: Udacity project for POC to Production Ready Code
+
+-----
+Date: 2024-1-3
+
+'''
+
+
+# import libraries
+import os
+import seaborn as sns
+from sklearn.metrics import classification_report, plot_roc_curve
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import joblib
+import shap 
+from constant import CAT_COLUMNS, KEEP_COLUMNS
+
+os.environ['QT_QPA_PLATFORM'] = 'offscreen'
+sns.set()
+
+
+def import_data(pth):
+    '''
+    returns dataframe for the csv found at pth
+
+    input:
+            pth: a path to the csv
+    output:
+            data_frame: pandas dataframe
+    '''
+    return pd.read_csv(pth)
+
+
+def perform_eda(data_frame, show_flag:bool=True):
+    '''
+    perform eda on data_frame and save figures to images folder
+    input:
+            data_frame: pandas dataframe
+
+    output:
+            None
+    '''
+
+    data_frame['Churn'] = data_frame['Attrition_Flag'].apply(
+        lambda val: 0 if val == "Existing Customer" else 1)
+
+    plt.figure(figsize=(20, 10))
+    data_frame['Churn'].hist()
+    plt.tight_layout()
+    plt.savefig(os.path.join("./images/eda", 'Test.png'))
+    if show_flag:
+        plt.show(block=False)
+
+    plt.figure(figsize=(20, 10))
+    data_frame['Customer_Age'].hist()
+    plt.tight_layout()
+    plt.savefig(os.path.join("./images/eda", 'CustomerAge.png'))
+    if show_flag:
+        plt.show(block=False)
+
+    plt.figure(figsize=(20, 10))
+    data_frame.Marital_Status.value_counts('normalize').plot(kind='bar')
+    plt.savefig(os.path.join("./images/eda", 'MaritalStatus.png'))
+
+    plt.figure(figsize=(20, 10))
+    sns.histplot(data_frame['Total_Trans_Ct'], stat='density', kde=True)
+    plt.savefig(os.path.join("./images/eda", 'TotalTransHist.png'))
+
+    plt.figure(figsize=(20, 10))
+    sns.heatmap(data_frame.corr(), annot=False, cmap='Dark2_r', linewidths=2)
+    plt.savefig(os.path.join("./images/eda", 'CorrHeatmap.png'))
+    if show_flag:
+        plt.show()
+
+
+def encoder_helper(data_frame, category_lst, response):
+    '''
+    helper function to turn each categorical column into a new column with
+    propotion of churn for each category - associated with cell 15 from the notebook
+
+    input:
+            data_frame: pandas dataframe
+            category_lst: list of columns that contain categorical features
+            response: string of response name [optional argument that could be used for
+                      naming variables or index y column]
+
+    output:
+            data_frame: pandas dataframe with new columns for
+    '''
+    for category in category_lst:
+        gender_groups = data_frame.groupby(category).mean()[response]
+        gender_lst = [gender_groups.loc[val] for val in data_frame[category]]
+        data_frame[f'{category}_Churn'] = gender_lst
+    return data_frame
+
+
+def perform_feature_engineering(data_frame, response: str = "Churn"):
+    '''
+    input:
+              data_frame: pandas dataframe
+              response: string of response name [optional argument that could be
+                        used for naming variables or index y column]
+
+    output:
+              X_train: X training data
+              X_test: X testing data
+              y_train: y training data
+              y_test: y testing data
+    '''
+    churn_axis = data_frame[response]
+
+    data_frame = encoder_helper(data_frame, CAT_COLUMNS, response)
+
+    proc_dataframe = pd.DataFrame()
+    proc_dataframe[KEEP_COLUMNS] = data_frame[KEEP_COLUMNS]
+
+    return train_test_split(proc_dataframe, churn_axis, test_size=0.3, random_state=42)
+
+
+def generate_classification_report(model_name,
+                                   y_train,
+                                   y_test,
+                                   y_train_preds,
+                                   y_test_preds):
+    '''
+    produces classification report for training and testing results and stores
+    report as image in images folder
+
+    input:
+                    model_name: (str) name of the model, ie 'Random Forest'
+                    y_train: training response values
+                    y_test:  test response values
+                    y_train_preds: training predictions from model_name
+                    y_test_preds: test predictions from model_name
+
+    output:
+                     None
+    '''
+    plt.rc('figure', figsize=(5, 5))
+
+    font_size = {'fontsize': 10}
+    # Plot Classification report on Train dataset
+    plt.text(0.01, 1.25, f'{model_name} Train', font_size)
+    plt.text(0.01, 0.05,
+             str(classification_report(y_train, y_train_preds)),
+             font_size)
+
+    # Plot Classification report on Test dataset
+    plt.text(0.01, 0.6, f'{model_name} Test', font_size)
+    plt.text(0.01, 0.7,
+             str(classification_report(y_test, y_test_preds)),
+             font_size
+             )
+
+    plt.axis('off')
+
+    # Save figure to ./images folder
+    plt.tight_layout()
+    plt.savefig(os.path.join("./images/results", f'Classification_report_{model_name}.png'))
+
+    plt.show()
+    plt.close()
+
+
+def classification_report_image(y_train,
+                                y_test,
+                                y_train_preds_lr,
+                                y_train_preds_rf,
+                                y_test_preds_lr,
+                                y_test_preds_rf):
+    '''
+    produces classification report for training and testing results and stores report as image
+    in images folder
+    input:
+            y_train: training response values
+            y_test:  test response values
+            y_train_preds_lr: training predictions from logistic regression
+            y_train_preds_rf: training predictions from random forest
+            y_test_preds_lr: test predictions from logistic regression
+            y_test_preds_rf: test predictions from random forest
+
+    output:
+             None
+    '''
+    generate_classification_report('Logistic_Regression',
+                                   y_train,
+                                   y_test,
+                                   y_train_preds_lr,
+                                   y_test_preds_lr)
+
+    generate_classification_report('Random_Forest',
+                                   y_train,
+                                   y_test,
+                                   y_train_preds_rf,
+                                   y_test_preds_rf)
+
+
+def feature_importance_plot(model, x_data, output_pth):
+    '''
+    creates and stores the feature importances in pth
+    input:
+            model: model object containing feature_importances_
+            X_data: pandas dataframe of X values
+            output_pth: path to store the figure
+
+    output:
+             None
+    '''
+    # Calculate feature importances
+    importances = model.feature_importances_
+    # Sort feature importances in descending order
+    indices = np.argsort(importances)[::-1]
+
+    # Rearrange feature names so they match the sorted feature importances
+    names = [x_data.columns[i] for i in indices]
+
+    # Create plot
+    plt.figure(figsize=(20, 5))
+
+    plt.title("Feature Importance")
+    plt.ylabel('Importance')
+
+    plt.bar(range(x_data.shape[1]), importances[indices])
+
+    # Add feature names as x-axis labels
+    plt.xticks(range(x_data.shape[1]), names, rotation=90)
+
+    # Save figure to output_pth
+    plt.tight_layout()
+    plt.savefig(os.path.join(output_pth, 'feature_importance.png'))
+
+    plt.show()
+    plt.close()
+
+
+def train_models(x_train, y_train):
+    '''
+    train, store model results: images + scores, and store models
+    input:
+              X_train: X training data
+              y_train: y training data
+    output:
+              None
+    '''
+    # grid search
+    rfc = RandomForestClassifier(random_state=42)
+    lrc = LogisticRegression(solver='lbfgs', max_iter=3000)
+
+    param_grid = {
+        'n_estimators': [200, 500],
+        'max_features': ['auto', 'sqrt'],
+        'max_depth': [4, 5, 100],
+        'criterion': ['gini', 'entropy']
+    }
+    cv_rfc = GridSearchCV(estimator=rfc, param_grid=param_grid, cv=5)
+    cv_rfc.fit(x_train, y_train)
+
+    lrc.fit(x_train, y_train)
+
+    joblib.dump(cv_rfc.best_estimator_, './models/rfc_model.pkl')
+    joblib.dump(lrc, './models/logistic_model.pkl')
+
+
+def predict_models(model, predict_input):
+    '''
+    train, store model results: images + scores, and store models
+    input:
+              model: model with which to do the prediciton
+              predict_input: input for prediction
+    output:
+              prediction
+    '''
+    return model.predict(predict_input)
+
+def evaluate_models(lrc_model, rfc_model,
+                    x_train, x_test,
+                    y_train, y_test,
+                    y_train_preds_lr, y_train_preds_rf,
+                    y_test_preds_lr, y_test_preds_rf):
+    '''
+    evaluate model results: images + scores
+    input:
+              lrc_model: logarithmic regression model
+              rfc_model: random forest model
+              x_train: X training data
+              x_test: X testing data
+              y_train: y training data
+              y_test: y testing data
+              y_train_preds_lr: y predicted data by log regression on training
+              y_train_preds_rf: y predicted data by log regression on training
+              y_test_preds_lr: y predicted data by log regression on test
+              y_test_preds_rf: y predicted data by log regression on test
+    output:
+              None
+    '''
+    # scores
+    classification_report_image(y_train, y_test,
+                                y_train_preds_lr, y_train_preds_rf,
+                                y_test_preds_lr, y_test_preds_rf)
+
+    lrc_plot = plot_roc_curve(lrc_model, x_test, y_test)
+
+    plt.figure(figsize=(15, 8))
+    ax_var = plt.gca()
+    plot_roc_curve(rfc_model, x_test, y_test, ax=ax_var, alpha=0.8)
+
+    lrc_plot.plot(ax=ax_var, alpha=0.8)
+    plt.show()
+    plt.tight_layout()
+    plt.savefig(os.path.join("./images/results", 'ROC_curves.png'))
+    plt.close()
+
+    # Display feature importance on train data
+    feature_importance_plot(model=rfc_model,
+                            x_data=x_train,
+                            output_pth="./images/results")
+
+    explainer = shap.TreeExplainer(rfc_model)
+    shap_values = explainer.shap_values(x_test)
+    shap.summary_plot(shap_values, x_test, plot_type="bar")
+    plt.savefig('./images/results/SHAP.png')
+
+if __name__ == "__main__":
+    print("Importing Data")
+    dataset = import_data("./data/bank_data.csv")
+    print("Perform EDA")
+    perform_eda(dataset, show_flag=False)
+    print("Feature Engineering")
+    x_train, x_test, y_train, y_test = perform_feature_engineering(dataset, response='Churn')
+    print("Train Models")
+    train_models(x_train, y_train)
+
+    print("Load Trained Models")
+    rfc_model = joblib.load('./models/rfc_model.pkl')
+    lr_model = joblib.load('./models/logistic_model.pkl')
+
+    print("Predict")
+    y_train_preds_rf = predict_models(rfc_model, x_train)
+    y_test_preds_rf = predict_models(rfc_model, x_test)
+
+    y_train_preds_lr = predict_models(lr_model, x_train)
+    y_test_preds_lr = predict_models(lr_model, x_test)
+
+    print("Evaluate")
+    evaluate_models(lr_model, rfc_model,
+                    x_train, x_test,
+                    y_train, y_test,
+                    y_train_preds_lr, y_train_preds_rf,
+                    y_test_preds_lr, y_test_preds_rf)
+
+
---
+++ churn_script_logging_and_tests.py
@@ -0,0 +1,229 @@
+'''
+
+File Name: churn_library.py
+
+Author(s): Sara Abanmi
+
+Brief Description: Udacity project for POC to Production Ready Code
+
+-----
+Date: 2024-1-3
+
+'''
+
+import os
+import logging
+import joblib
+import churn_library as cls
+
+from constant import CAT_COLUMNS
+
+logging.basicConfig(
+    filename='./logs/churn_library.log',
+    level=logging.INFO,
+    filemode='w',
+    format='%(name)s - %(levelname)s - %(message)s')
+
+
+def test_import(import_data):
+    '''
+    test data import - this example is completed for you to assist with the other test functions
+    '''
+    print("Test Import")
+    data_frame = None
+    try:
+        data_frame = import_data("./data/bank_data.csv")
+        assert data_frame.shape[0] > 0
+        assert data_frame.shape[1] > 0
+    except FileNotFoundError as err:
+        logging.error("Testing import_eda execution: The file wasn't found")
+        raise err
+    except AssertionError as err:
+        logging.error(
+            "Testing import_data functionnality: The file doesn't appear to have rows and columns")
+        raise err
+    else:
+        logging.info("Testing import_data functionnality: SUCCESS")
+        return data_frame
+
+
+def test_eda(perform_eda, dataframe):
+    '''
+    test perform eda function
+    '''
+    print("Test EDA")
+    try:
+        perform_eda(dataframe, False)
+        assert os.path.exists("./images/eda/test.png")
+        assert os.path.exists("./images/eda/Customer_Age.png")
+    except AssertionError as err:
+        logging.error("Testing perform_eda functionnality: Files not created")
+        raise err
+    except Exception as err:
+        logging.error("Testing perform_eda execution: Issue encountered")
+        raise err
+    else:
+        logging.info("Testing perform_eda functionnality: SUCCESS")
+
+
+def test_encoder_helper(encoder_helper, data_frame, cat_column, response):
+    '''
+    test encoder helper
+    '''
+    print("Test Encoder Helper")
+    try:
+        data_frame_out = encoder_helper(data_frame, cat_column, response)
+        assert len(data_frame) > 0
+    except AssertionError as err:
+        logging.error(
+            "Testing encoder_helper functionnality: output data frame empty")
+        raise err
+    except Exception as err:
+        logging.error("Testing encoder_helper execution: Issue encountered")
+        raise err
+    else:
+        logging.info("Testing encoder_helper functionnality: SUCCESS")
+
+
+def test_perform_feature_engineering(perform_feature_engineering, dataset):
+    '''
+    test perform_feature_engineering
+    '''
+    print("Test Feature Engineering")
+    x_train, x_test, y_train, y_test = None, None, None, None
+    try:
+        x_train, x_test, y_train, y_test = perform_feature_engineering(
+            dataset, response='Churn')
+        assert len(x_train) > 0
+        assert len(x_test) > 0
+        assert len(y_train) > 0
+        assert len(y_test) > 0
+    except AssertionError as err:
+        logging.error(
+            "Testing perform_feature_engineering functionnality: one "
+            "of the output data frame is empty")
+        raise err
+    except Exception as err:
+        logging.error(
+            "Testing perform_feature_engineering execution: Issue encountered")
+        raise err
+    else:
+        logging.info(
+            "Testing perform_feature_engineering functionnality: SUCCESS")
+        return x_train, x_test, y_train, y_test
+
+
+def test_train_models(train_models, x_train, y_train):
+    '''
+    test train_models
+    '''
+    print("Test Train Models")
+    try:
+        train_models(x_train, y_train)
+        assert os.path.exists("./models/rfc_model.pkl")
+        assert os.path.exists("./models/logistic_model.pkl")
+    except AssertionError as err:
+        logging.error(
+            "Testing train_models functionnality: one of the output data "
+            "frame is empty")
+        raise err
+    except Exception as err:
+        logging.error(
+            "Testing train_models execution: Issue encountered")
+        raise err
+    else:
+        logging.info(
+            "Testing train_models functionnality: SUCCESS")
+
+def test_predict_models(predict_models, model, predict_input):
+    '''
+    test predict_models
+    '''
+    predict_values = None
+    print("Test Predict Models")
+    try:
+        predict_values = predict_models(model, predict_input)
+        assert len(predict_values)==len(predict_input)
+    except AssertionError as err:
+        logging.error(
+            "Testing predict_models functionnality: one of the output data "
+            "frame is empty")
+        raise err
+    except Exception as err:
+        logging.error(
+            "Testing predict_models execution: Issue encountered")
+        raise err
+    else:
+        logging.info(
+            "Testing predict_models functionnality: SUCCESS")
+        return predict_values
+
+
+
+def test_evaluate_models(evaluate_models, lr_model, rfc_model,
+                    x_train, x_test,
+                    y_train, y_test,
+                    y_train_preds_lr, y_train_preds_rf,
+                    y_test_preds_lr, y_test_preds_rf):
+    '''
+    test evaluate_models
+    '''
+    print("Test Evaluate Models")
+    try:
+        evaluate_models(lr_model, rfc_model,
+                    x_train, x_test,
+                    y_train, y_test,
+                    y_train_preds_lr, y_train_preds_rf,
+                    y_test_preds_lr, y_test_preds_rf)
+        assert os.path.exists(
+            "./images/results/Classification_report_Logistic_Regression.png")
+        assert os.path.exists(
+            "./images/results/Classification_report_Random_Forest.png")
+        assert os.path.exists("./images/results/ROC_curves.png")
+        assert os.path.exists("./images/results/feature_importance.png")
+    except AssertionError as err:
+        logging.error(
+            "Testing evaluate_models functionnality: one of the output data "
+            "frame is empty")
+        raise err
+    except Exception as err:
+        logging.error(
+            "Testing evaluate_models execution: Issue encountered")
+        raise err
+    else:
+        logging.info(
+            "Testing evaluate_models functionnality: SUCCESS")
+        return x_train, x_test, y_train, y_test
+
+
+if __name__ == "__main__":
+    data_frame = test_import(cls.import_data)
+
+    test_eda(cls.perform_eda, data_frame)
+
+    test_encoder_helper(cls.encoder_helper,
+                        data_frame,
+                        CAT_COLUMNS,
+                        "Churn")
+
+    x_train, x_test, y_train, y_test = test_perform_feature_engineering(
+        cls.perform_feature_engineering,
+        data_frame)
+
+    test_train_models(cls.train_models, x_train, y_train)
+
+    rfc_model = joblib.load('./models/rfc_model.pkl')
+    lr_model = joblib.load('./models/logistic_model.pkl')
+
+    y_train_preds_rf = test_predict_models(cls.predict_models, rfc_model, x_train)
+    y_test_preds_rf = test_predict_models(cls.predict_models, rfc_model, x_test)
+
+    y_train_preds_lr = test_predict_models(cls.predict_models, lr_model, x_train)
+    y_test_preds_lr = test_predict_models(cls.predict_models, lr_model, x_test)
+
+    test_evaluate_models(cls.evaluate_models,
+                         lr_model, rfc_model,
+                         x_train, x_test,
+                         y_train, y_test,
+                         y_train_preds_lr, y_train_preds_rf,
+                         y_test_preds_lr, y_test_preds_rf)
